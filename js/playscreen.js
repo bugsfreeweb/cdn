b9a10254fcb8c83ea9152b941bf3c56104fa2967
Bugsfree <113718082+bugsfreeweb@users.noreply.github.com>
2022-11-25 23:21:07 +0600

    Add files via upload

--- js/playscreen.js
+++ js/playscreen.js
@@ -1,17 +1,17 @@
-let LosCanales = [
+let ChannelLists = [
     {
-        'Title': 'Bangla TV',
-        'Name': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://banglatv.tv/wp-content/uploads/2018/10/bangla-tv-logo.png"></img>',
+        'title': 'Bangla TV',
+        'name': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://banglatv.tv/wp-content/uploads/2018/10/bangla-tv-logo.png"></img>',
         'url': 'https://bugsfreecdn.netlify.app/playscreen/banglatv.html'
     },
 	{
-        'Title': 'News24',
-        'Name': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://logovectordl.com/wp-content/uploads/2019/12/news24bd-tv-logo-vector.png"></img>',
+        'title': 'News24',
+        'name': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://logovectordl.com/wp-content/uploads/2019/12/news24bd-tv-logo-vector.png"></img>',
         'url': 'https://bugsfreecdn.netlify.app/playscreen/news24.html'
     },
 	{
-        'Title': 'Boishakhi Tv',
-        'Name': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c7/Boishakhi_TV_logo.svg/1200px-Boishakhi_TV_logo.svg.png"></img>',
+        'title': 'Boishakhi Tv',
+        'name': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c7/Boishakhi_TV_logo.svg/1200px-Boishakhi_TV_logo.svg.png"></img>',
         'url': 'https://bugsfreecdn.netlify.app/playscreen/boishakhitv.html'
     },
 	
