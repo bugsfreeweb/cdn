667270d1e4947687faf92e8fd4c52b26e1aaa04f
Bugsfree <113718082+bugsfreeweb@users.noreply.github.com>
2022-11-26 00:06:39 +0600

    Add files via upload

--- js/playscreen.js
+++ js/playscreen.js
@@ -1,17 +1,17 @@
 let LosCanales = [
     {
-        'title': 'Bangla TV',
-        'name': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://banglatv.tv/wp-content/uploads/2018/10/bangla-tv-logo.png"></img>',
+        'titulo': 'Bangla TV',
+        'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://banglatv.tv/wp-content/uploads/2018/10/bangla-tv-logo.png"></img>',
         'url': 'https://bugsfreecdn.netlify.app/playscreen/banglatv.html'
     },
 	{
-        'title': 'News24',
-        'name': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://logovectordl.com/wp-content/uploads/2019/12/news24bd-tv-logo-vector.png"></img>',
+        'titulo': 'News24',
+        'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://logovectordl.com/wp-content/uploads/2019/12/news24bd-tv-logo-vector.png"></img>',
         'url': 'https://bugsfreecdn.netlify.app/playscreen/news24.html'
     },
 	{
-        'title': 'Boishakhi Tv',
-        'name': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c7/Boishakhi_TV_logo.svg/1200px-Boishakhi_TV_logo.svg.png"></img>',
+        'titulo': 'Boishakhi Tv',
+        'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c7/Boishakhi_TV_logo.svg/1200px-Boishakhi_TV_logo.svg.png"></img>',
         'url': 'https://bugsfreecdn.netlify.app/playscreen/boishakhitv.html'
     },
 	
