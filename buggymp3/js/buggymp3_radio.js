fceb5f347ffaab1a0bfbaf5a14ae40613a73acdd
Bugsfree <113718082+bugsfreeweb@users.noreply.github.com>
2023-03-14 22:11:30 +0600

    Update buggymp3_radio.js

--- buggymp3/js/buggymp3_radio.js
+++ buggymp3/js/buggymp3_radio.js
@@ -3,30 +3,30 @@ const songs = [
         name: 'Bugsfree Radio',
         path: 'https://remixzilla.com/siteuploads/files/sfd11/5089/Hindi%20Love%20All%20Time%20Hits%20Nonstop%20DJ%20Song%20Collection%202022(RemixZilla.Com).mp3',
         artist: 'Bugsfre Default Radio',
-        cover: 'buggymp3/imgs/radio_main.png'
+        cover: 'https://bugsfreecdn.netlify.app/buggymp3/imgs/radio_main.png'
     },
     {
         name: 'NonStop PartyMix',
         path: 'https://remixzilla.com/siteuploads/files/sfd11/5115/Best%20Nonstop%20Party%20Mix%20Mashup%20Bollywood%20Dj%20Remix%20Song%202022(RemixZilla.Com).mp3',
         artist: 'PartyMix',
-        cover: 'buggymp3/imgs/radio_main_01.png'
+        cover: 'https://bugsfreecdn.netlify.app/buggymp3/imgs/radio_main_01.png'
     },
     {
         name: 'Romantic Bibe',
         path: 'https://remixzilla.com/siteuploads/files/sfd11/5060/Romantic%20Vibes%20Best%20Of%20Love%202022%20Nonstop%20Jukebox%20Mashup(RemixZilla.Com).mp3',
         artist: 'Romantic Songs',
-        cover: 'buggymp3/imgs/radio_main_02.png'
+        cover: 'https://bugsfreecdn.netlify.app/buggymp3/imgs/radio_main_02.png'
     },
     {
         name: 'Broken Heart',
         path: 'https://remixzilla.com/siteuploads/files/sfd10/4729/Heart%20Broken%20Chillout%20Sad%20Mashup%20Songs%202021(RemixZilla.Com).mp3',
         artist: 'Sorrow Mix',
-        cover: 'buggymp3/imgs/radio_main_03.png'
+        cover: 'https://bugsfreecdn.netlify.app/buggymp3/imgs/radio_main_03.png'
     },
     {
         name: 'Justin Bieber Anyone',
         path: 'https://remixzilla.com/siteuploads/files/sfd9/4295/Justin%20Bieber%20-%20Anyone%20(Jack%20Shore%20Remix)%202021(RemixZilla.Com).mp3',
         artist: 'Jack Shore Remix-2021',
-        cover: 'buggymp3/imgs/radio_main_04.png'
+        cover: 'https://bugsfreecdn.netlify.app/buggymp3/imgs/radio_main_04.png'
     }
 ]
