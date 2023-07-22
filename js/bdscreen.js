04ad55f9b2caf1e5cc3405bb3e4191218318b7f7
Bugsfree <113718082+bugsfreeweb@users.noreply.github.com>
2023-07-22 23:33:36 +0600

    Update bdscreen.js

--- js/bdscreen.js
+++ js/bdscreen.js
@@ -19,26 +19,13 @@ let LosCanales = [
         'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://thedailynewnation.com/library/1396623976_0.jpg"></img>',
         'url': 'https://bugsfreecdn.netlify.app/playscreen/jamunatv.html'
     },
-	{
-        'titulo': 'Ekushey TV',
-        'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d9/Ekushey_Television_Logo.svg/1200px-Ekushey_Television_Logo.svg.png"></img>',
-        'url': 'https://bugsfreecdn.netlify.app/playscreen/ekusheytv.html'
-    },
-	{
-        'titulo': 'Nandan TV',
-        'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://play-lh.googleusercontent.com/NFY7IIuJrsa9S3t2zkOCwn4yiwVAxBRauvwQlpjlmsrIOvBhhkj9oT50-qqhpPPKbgQ=w240-h480-rw"></img>',
-        'url': 'https://bugsfreecdn.netlify.app/playscreen/nandantv.html'
-    },
+	
 	{
         'titulo': 'Nan TV',
         'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://yt3.ggpht.com/EUfH-zm--KHbNr1vUG6MP7QLaH1Yeutc7w-doCn7Ty1b2w0kKvaV_tkjCVIliYkpqiMAIT7EEx8=s900-c-k-c0x00ffffff-no-rj"></img>',
         'url': 'https://bugsfreecdn.netlify.app/playscreen/nantv.html'
     },
-	{
-        'titulo': 'Sangeet Bangla',
-        'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://static.wikia.nocookie.net/logopedia/images/1/1f/Sangeet_Bangla.jpg/revision/latest?cb=20200804091515"></img>',
-        'url': 'https://bugsfreecdn.netlify.app/playscreen/sangeet_bangla.html'
-    },
+	
 	{
         'titulo': 'Ananda TV',
         'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://upload.wikimedia.org/wikipedia/en/0/02/Ananda_TV_Logo.png"></img>',
