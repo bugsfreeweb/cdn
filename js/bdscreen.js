3e1028cee1a7781b26c8caf158dcd908fae3137f
Bugsfree <113718082+bugsfreeweb@users.noreply.github.com>
2023-07-22 23:31:23 +0600

    Update bdscreen.js

--- js/bdscreen.js
+++ js/bdscreen.js
@@ -1,17 +1,57 @@
 let LosCanales = [
     {
-        'titulo': 'Gazi TV',
-        'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="http://amrbd.com/assets/images/gazi.jpg"></img>',
-        'url': 'https://bugsfreecdn.netlify.app/playscreen/gtv.html'
-    },
-	{
-        'titulo': 'T-sports TV',
-        'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="http://amrbd.com/assets/images/tsports.jpg"></img>',
-        'url': 'https://bugsfreecdn.netlify.app/playscreen/tsports.html'
-    },
-	{
         'titulo': 'Bangla TV',
         'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://banglatv.tv/wp-content/uploads/2018/10/bangla-tv-logo.png"></img>',
         'url': 'https://bugsfreecdn.netlify.app/playscreen/banglatv.html'
-    },		
+    },
+	{
+        'titulo': 'ATN Bangla',
+        'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/ATN_Bangla.svg/2560px-ATN_Bangla.svg.png"></img>',
+        'url': 'https://bugsfreecdn.netlify.app/playscreen/atn_bangla.html'
+    },
+	{
+        'titulo': 'Channel 24',
+        'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Channel24logo.svg/1200px-Channel24logo.svg.png"></img>',
+        'url': 'https://bugsfreecdn.netlify.app/playscreen/channel24.html'
+    },
+	{
+        'titulo': 'Jamuna TV',
+        'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://thedailynewnation.com/library/1396623976_0.jpg"></img>',
+        'url': 'https://bugsfreecdn.netlify.app/playscreen/jamunatv.html'
+    },
+	{
+        'titulo': 'Ekushey TV',
+        'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d9/Ekushey_Television_Logo.svg/1200px-Ekushey_Television_Logo.svg.png"></img>',
+        'url': 'https://bugsfreecdn.netlify.app/playscreen/ekusheytv.html'
+    },
+	{
+        'titulo': 'Nandan TV',
+        'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://play-lh.googleusercontent.com/NFY7IIuJrsa9S3t2zkOCwn4yiwVAxBRauvwQlpjlmsrIOvBhhkj9oT50-qqhpPPKbgQ=w240-h480-rw"></img>',
+        'url': 'https://bugsfreecdn.netlify.app/playscreen/nandantv.html'
+    },
+	{
+        'titulo': 'Nan TV',
+        'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://yt3.ggpht.com/EUfH-zm--KHbNr1vUG6MP7QLaH1Yeutc7w-doCn7Ty1b2w0kKvaV_tkjCVIliYkpqiMAIT7EEx8=s900-c-k-c0x00ffffff-no-rj"></img>',
+        'url': 'https://bugsfreecdn.netlify.app/playscreen/nantv.html'
+    },
+	{
+        'titulo': 'Sangeet Bangla',
+        'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://static.wikia.nocookie.net/logopedia/images/1/1f/Sangeet_Bangla.jpg/revision/latest?cb=20200804091515"></img>',
+        'url': 'https://bugsfreecdn.netlify.app/playscreen/sangeet_bangla.html'
+    },
+	{
+        'titulo': 'Ananda TV',
+        'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://upload.wikimedia.org/wikipedia/en/0/02/Ananda_TV_Logo.png"></img>',
+        'url': 'https://bugsfreecdn.netlify.app/playscreen/anandatv.html'
+    },
+	{
+        'titulo': 'News24',
+        'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://logovectordl.com/wp-content/uploads/2019/12/news24bd-tv-logo-vector.png"></img>',
+        'url': 'https://bugsfreecdn.netlify.app/playscreen/news24.html'
+    },
+	{
+        'titulo': 'Boishakhi Tv',
+        'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c7/Boishakhi_TV_logo.svg/1200px-Boishakhi_TV_logo.svg.png"></img>',
+        'url': 'https://bugsfreecdn.netlify.app/playscreen/boishakhitv.html'
+    },	
 ]
