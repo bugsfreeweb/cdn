10ebd103678ee6e37573f4ac9708c121931ea0d1
Bugsfree <113718082+bugsfreeweb@users.noreply.github.com>
2023-07-22 23:29:49 +0600

    Update bdscreen.js

--- js/bdscreen.js
+++ js/bdscreen.js
@@ -13,12 +13,5 @@ let LosCanales = [
         'titulo': 'Bangla TV',
         'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://banglatv.tv/wp-content/uploads/2018/10/bangla-tv-logo.png"></img>',
         'url': 'https://bugsfreecdn.netlify.app/playscreen/banglatv.html'
-    },
-	
-	{
-        'titulo': 'Nasa TV',
-        'nombre': '<span style="margin-right: 10px;" class="material-icons">tv</span><img style="height: 20px; width: auto; margin-right: 10px;" src="https://www.nasa.gov/sites/default/files/images/nasaLogo-570x450.png"></img>',
-        'url': 'https://bugsfreecdn.netlify.app/playscreen/nasatv.html'
-    },
-	
+    },		
 ]
