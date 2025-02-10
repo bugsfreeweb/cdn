3ac7092cf79606050fb9ecfd6977029a7fb9da5c
Bugsfree <113718082+bugsfreeweb@users.noreply.github.com>
2025-02-10 13:16:34 +0600

    Update fullscreen.js

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,87 +1,25 @@
-// you must uncheck "show address bar" in the page of "vivaldi://settings/addressbar/"
-// Do not set F11 in 'Fullscreen Mode'
-// this script set "Alt+X" to toggle address bar and tab bar
-
-let hr = setInterval(() => {
-  const c = document.querySelector("#webview-container");
-  const header = document.querySelector("#header");
-  const browser = document.querySelector("#browser");
-  let fullscreenEnabled = false;
-
-  function show() {
-    if (!fullscreenEnabled) {
-      return;
-    }
-    header.hidden = false;
-    [...document.querySelectorAll(".tabbar-wrapper")].forEach(
-      (item) => (item.hidden = false)
-    );
-    browser.classList.remove("address-top-off");
-    browser.classList.add("address-top");
+function enterFullScreen(element) {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  }else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();     // Firefox
+  }else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();  // Safari
+  }else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();      // IE/Edge
   }
+};
 
-  function toggle() {
-    if (!fullscreenEnabled) {
-      return;
-    }
-    browser.classList.contains("address-top") ? hide() : show();
+let btn = document.getElementById("full_screen");
+btn.addEventListener("click", function(){
+  let videoEle = document.querySelector('video');
+  enterFullScreen(videoEle);
+});
+
+document.addEventListener('fullscreenchange', (event) => {
+  if (document.fullscreenElement) {
+    console.log('Entered fullscreen:', document.fullscreenElement);
+  } else {
+    console.log('Exited fullscreen.');
   }
-
-  if (c) {
-    const style = document.createElement("style");
-    style.appendChild(
-      document.createTextNode(`[hidden] { display: none !important; }`)
-    );
-    document.head.appendChild(style);
-
-    vivaldi.tabsPrivate.onKeyboardShortcut.addListener((id, combination) => {
-      if(combination === "F11"){
-        if(fullscreenEnabled){
-          show();
-        }else{
-          hide(true)
-        }
-        setTimeout(()=>{
-          fullscreenEnabled = !fullscreenEnabled;
-        },111);
-      }
-      combination === "Alt+X" && toggle();
-    });
-
-    clearInterval(hr);
-    const div = document.createElement("div");
-    div.style.height = "9px";
-    div.style.width = "100vw";
-    div.style.position = "fixed";
-    div.style.left = "0";
-    div.style.top = "0";
-    div.style.zIndex = 1;
-    document.body.insertBefore(div, document.body.firstChild);
-
-    const div2 = document.createElement("div");
-    div2.style.height = "100vh";
-    div2.style.width = "8px";
-    div2.style.position = "fixed";
-    div2.style.left = "0";
-    div2.style.top = "0";
-    div2.style.zIndex = 2;
-    document.body.insertBefore(div2, document.body.firstChild);
-
-    function hide(force) {
-      if (!force && !fullscreenEnabled) {
-        return;
-      }
-      header.hidden = true;
-      browser.classList.remove("address-top");
-      browser.classList.add("address-top-off");
-      [...document.querySelectorAll(".tabbar-wrapper")].forEach(
-        (item) => (item.hidden = true)
-      );
-    }
-    c.addEventListener("pointerenter", () => {
-      hide();
-    });
-    div.addEventListener("pointerenter", show);
-    div2.addEventListener("pointerenter", show);
-  }
-}, 1111);
+});
